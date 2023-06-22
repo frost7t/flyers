7c0ecf60299dabfedbe1ffeeb5578860d8cc3155
Antoine <raimbaultantoine1@gmail.com>
2023-06-23 00:56:59 +0200

    exp/done

--- src/Components/buttons/ButtonExp.js
+++ src/Components/buttons/ButtonExp.js
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default function ButtonExp() {
   return (
     <div>
-      <button className="bg-black hover:bg-[#252424] text-white px-4 py-2 lg:px-5 lg:py-5 rounded-full">
-        Book an Aircraft
-      </button>
+      <Link href="/">
+        <button className="bg-black hover:bg-[#252424] font-medium text-lg text-[#fff] px-4 py-2 lg:px-8 lg:py-4 rounded-full">
+          Book an Aircraft
+        </button>
+      </Link>
     </div>
   );
 }
