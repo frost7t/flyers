714891cba082bc78a1719b0dc05c90bd7523be0a
Antoine <raimbaultantoine1@gmail.com>
2023-06-29 04:56:58 +0200

    index/done. start_about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import Footer from "@/Components/footer/Footer";
+import OtherNav from "@/Components/navigation/OtherNav";
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="bg-[#8AADB2]">
+      <div>
+        <OtherNav />
+        <Footer />
+      </div>
+    </div>
+  );
 }
--- src/pages/solutions.js
+++ src/pages/solutions.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Solutions() {
   return (
-    <div>Solutions</div>
-  )
+    <div>
+      solutions
+    </div>
+  );
 }
