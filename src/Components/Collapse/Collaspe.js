18708111c99095c255b52c49a08f2e579de81918
Antoine <raimbaultantoine1@gmail.com>
2023-07-22 17:39:46 +0200

    Collaspe_doing

--- src/Components/Collapse/Collaspe.js
+++ src/Components/Collapse/Collaspe.js
@@ -1,7 +1,79 @@
 import React from "react";
+import { Collapse, Text } from "@nextui-org/react";
 
 export default function Collaspe() {
-  return <div className="mx-auto max-w-[90%] py-10">
-    
-  </div>;
+  return (
+    <div className="mx-auto max-w-[90%] py-10">
+      <Collapse.Group>
+        <Collapse title="Covid-19" className="font-bold text-2xl lg:text-4xl">
+          <Text className="font-normal  lg:max-w-[41.6vw] lg:ml-[45.8vw]">
+            As part of the ongoing COVID-19 pandemic, Hyer® only offers aircraft
+            from operators that follow strict hygiene guidelines. Those
+            operators have put measures in place to prevent the spread of the
+            virus. This includes thoroughly disinfection of the aircraft cabin
+            before and after the flight. Apart from that, the flight crew have
+            to wear mouth masks at all times during the flight and they will
+            keep a safe distance from the passengers. In preparation for your
+            journey, your Flight Manager will check the current travel
+            restrictions at your destination and advise you if any travel
+            regulations are in place. Flying private with Hyer® is one of the
+            safest ways to travel during these times.
+          </Text>
+        </Collapse>
+        <Collapse
+          title="Airport Transfer"
+          className="font-bold text-2xl lg:text-4xl"
+        >
+          <Text className="font-normal lg:max-w-[41.6vw] lg:ml-[45.8vw]">
+            With Hyer® you can easily book luxurious ground transportation
+            before and after your flight. You enjoy a wide range of vehicles to
+            complement your travel experience. Larger buses, vans and coaches
+            are available to transport passengers flying with groups. The
+            drivers will always be waiting for you in the dedicated VIP
+            terminal. You will encounter a comfortable vehicle with leather
+            seats and generous legroom. You won’t need to tell your driver where
+            to go and which time you will arrive - they will already know. Count
+            on the support of experienced drivers to help to carry your luggage
+            to the car and to the doorstep of your arrival point. Hyer® has
+            partnered up with leading local transportation companies to ensure a
+            safe and comfortable journey. Our partners are accredited and local
+            regulated companies with a proven track record. Rest assured your
+            driver knows the way!
+          </Text>
+        </Collapse>
+        <Collapse title="Luggage" className="font-bold text-2xl lg:text-4xl">
+          <Text className="font-normal lg:max-w-[41.6vw] lg:ml-[45.8vw]">
+            When flying private, you enjoy considerable flexibility for flying
+            with luggage. As standard, you receive a very generous luggage
+            allowance when booking a private jet. At Hyer®, your Flight Manager
+            will ask you about the amount and type of luggage that you would
+            like to bring on board. With this information, the Flight Manager
+            Team check if there is any limitation or restriction applicable and
+            they will assign the most suitable aircraft to you. In terms of
+            luggage content, there is no limitation on how much liquids you can
+            carry with you. If there is any special or fragile cargo that needs
+            to be transported, your Flight Manager will ensure that extra
+            measures are taken to deal with your precious belongings. For our
+            shared flights, different rules apply.
+          </Text>
+        </Collapse>
+        <Collapse
+          title="Flying with pets"
+          className="font-bold text-2xl lg:text-4xl"
+        >
+          <Text className="font-normal lg:max-w-[41.6vw] lg:ml-[45.8vw]">
+            Do you want to fly with your pet? No problem! Hyer® offers the
+            possibility to bring your pet on board. A suitable aircraft, pet
+            friendly, will be assigned for your flight. You avoid the stress and
+            the worries of your pet being placed on an aircraft cargo hold,
+            surrounded by heavy luggage and cargo. As a standard, when flying
+            private, your pet will need to be transported in animal carriages
+            boxes. However, they will be flying next to you in the aircraft's
+            main cabin. Oh, and besides, we might even have a small treat for
+            them.
+          </Text>
+        </Collapse>
+      </Collapse.Group>
+    </div>
+  );
 }
