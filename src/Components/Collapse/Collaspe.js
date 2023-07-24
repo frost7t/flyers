a18a3ec7398c292acd28530f2bf6577ffc01e7bf
Antoine <raimbaultantoine1@gmail.com>
2023-07-24 11:35:59 +0200

    coms_Collaspe

--- src/Components/Collapse/Collaspe.js
+++ src/Components/Collapse/Collaspe.js
@@ -3,13 +3,16 @@ import { Collapse, Text } from "@nextui-org/react";
 import CollapseData from "@/data/CollapseData";
 import BookFlight from "../BookFlight";
 
-export default function Collaspe() {
+export default function CollapseComponent() {
+  // Utilisation de la méthode map
   const collapseItems = CollapseData.map((item, index) => (
     <Collapse
       key={index}
+      // Le titre de l'élément Collapse est défini à partir de la propriété "title" de chaque élément dans "CollapseData".
       title={item.title}
       className="font-bold text-2xl lg:text-4xl"
     >
+      {/* Le contenu de l'élément Collapse est défini ici à partir de la propriété "content" de chaque élément dans "CollapseData". */}
       <Text className="font-normal lg:max-w-[41.6vw] lg:ml-[45.8vw]">
         {item.content}
       </Text>
@@ -18,6 +21,7 @@ export default function Collaspe() {
   return (
     <>
       <div className="mx-auto max-w-[90%] py-10 border-b-2 border-[#000] mb-10">
+        {/* On regroupe ici tous les éléments Collapse générés précédemment, formant ainsi un ensemble d'accordéons. */}
         <Collapse.Group>{collapseItems}</Collapse.Group>
       </div>
       <div className="lg:pt-20">
