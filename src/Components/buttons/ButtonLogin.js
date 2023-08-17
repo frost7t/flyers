cd96382aaf66e05adf81dc1ace07d471642dd8bc
Antoine <raimbaultantoine1@gmail.com>
2023-08-17 19:05:09 +0200

    re:directory_to_404

--- src/Components/buttons/ButtonLogin.js
+++ src/Components/buttons/ButtonLogin.js
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import React from 'react'
 
 export default function ButtonLogin() {
   return (
     <div className="py-5">
-      <button className="bg-[#000] hover:bg-[#000000cc] text-[#fff] text-xl lg:text-base font-semibold px-8 py-2 lg:px-5 lg:py-3 rounded-full">
-        Sign in
-      </button>
+      <Link href="/404">
+         {/*in progress --> directory 404  */}
+        <button className="bg-[#000] hover:bg-[#000000cc] text-[#fff] text-xl lg:text-base font-semibold px-8 py-2 lg:px-5 lg:py-3 rounded-full">
+          Sign in
+        </button>
+      </Link>
     </div>
   );
 }
